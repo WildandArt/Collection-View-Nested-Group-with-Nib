754b7b7aef3b852b32bba68d8037311de6a02559
Art <WildandArt@users.noreply.github.com>
2022-06-19 13:30:13 +0300

    Initial Commit

---
+++ Collection View Nested Group with Nib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection View Nested Group with Nib
+//
+//  Created by Artemy Ozerski on 19/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
